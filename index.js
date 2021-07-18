ef56e139a70047097bf5f774a8805addbb875e95
Permadi Wibisono <mbapewe@gmail.com>
2021-07-18 22:52:55 +0700

    Setting up dotenv, hpp & rate limiter

--- index.js
+++ index.js
@@ -3,20 +3,36 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require('morgan');
 const helmet = require('helmet');
-const dayjs = require('dayjs');
+const rateLimit = require("express-rate-limit");
+const hpp = require('hpp');
+const dotenv = require('dotenv');
 const app = express();
+const dayjs = require('dayjs');
 require('dayjs/locale/id');
 dayjs.locale('id');
 
+dotenv.config();
+app.enable("trust proxy");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+debug("cors enabled");
 app.use(helmet());
+debug("helmet enabled");
+app.use(hpp());
+debug("hpp enabled");
 
+app.use(morgan("[:date[iso]] :method :url :status :res[content-length] - :response-time ms"));
+debug("morgan enabled");
 
-if(app.get("env") === 'development') {
-  app.use(morgan("[:date[iso]] :method :url :status :res[content-length] - :response-time ms"));
-  debug("Morgan enabled");
+if(app.get("env") === "production") {
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+  });
+  //  apply to all requests
+  app.use(limiter);
+  debug("express-rate-limit enabled");
 }
 
 const PORT = process.env.PORT || 5000;
