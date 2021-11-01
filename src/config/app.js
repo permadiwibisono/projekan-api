d447192ff1cf42837ec3cdd73e2f19c4b5d6265e
Permadi Wibisono <mbapewe@gmail.com>
2021-11-01 20:03:00 +0700

    Add `key` in appConfig

--- src/config/app.js
+++ src/config/app.js
@@ -1,4 +1,10 @@
+import "dotenv/config";
+
+if (!process.env.APP_KEY || process.env.APP_KEY.length < 32) {
+  throw new Error("`APP_KEY` is required and contains at least 32 characters");
+}
 export const appConfig = {
+  key: process.env.APP_KEY,
   env: process.env.NODE_ENV || "development",
   stage: process.env.APP_STAGE || "local",
 };
