cab86d35e8afbe94a98c288bd74a2f299664178a
Permadi Wibisono <mbapewe@gmail.com>
2021-11-04 17:37:55 +0700

    Add verifyHash and passwordHash from argon2

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,6 +1,17 @@
 import crypto from "crypto";
+import argon2 from "argon2";
 import { appConfig } from "../config";
 import { randomStr } from "./strings";
+
+export const verifyHash = async (plainText, hash) => {
+  const result = await argon2.verify(plainText, hash);
+  return result;
+};
+
+export const passwordHash = async (plainText) => {
+  const result = await argon2.hash(plainText);
+  return result;
+};
 
 export const generateKey = (text) => {
   const rand = randomStr(4);
