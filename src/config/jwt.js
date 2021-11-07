69ddf7b6bf8266dc2a361f941af736a73255e364
Permadi Wibisono <mbapewe@gmail.com>
2021-11-08 01:55:20 +0700

    Update jwt config

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -3,8 +3,8 @@ import "dotenv/config";
 if (!process.env.JWT_SECRET) {
   throw new Error("`JWT_SECRET` is required");
 }
-const expiresIn = 60 * 3;
-// const expiresIn = 3600 * 24 * 7;
+// const expiresIn = 60 * 3;
+const expiresIn = 3600 * 24 * 7;
 
 export const jwtConfig = {
   issuer: process.env.JWT_ISSUER || "http://localhost:5000",
